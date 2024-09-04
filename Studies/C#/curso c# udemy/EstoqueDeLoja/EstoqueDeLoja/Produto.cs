66b790e5da4b76cdd1fef9e34ad50ea96bf457d1
Gabriel Machado <137454944+GabrielGMachado@users.noreply.github.com>
2024-09-04 10:57:45 -0300

    Add files via upload

---
+++ Studies/C#/curso c# udemy/EstoqueDeLoja/EstoqueDeLoja/Produto.cs
@@ -0,0 +1,37 @@
+﻿namespace EstoqueDeLoja {
+    internal class Produto {
+        public string nome;
+        public double valor;
+        public int quantidade;
+
+        public void DadosDoProduto() {
+            Console.WriteLine($"Dados do produto: Nome: {nome}, Valor total: {valor * quantidade}, Quantidade: {quantidade}");
+        }
+
+        public void AdicionarProdutos() {
+            Console.WriteLine("Deseja adicionar quantos produtos?");
+            int add = int.Parse(Console.ReadLine());
+            if (add < 0) {
+                Console.WriteLine("A quantidade a ser adicionada não pode ser negativa.");
+            }
+            else {
+                quantidade += add;
+                DadosDoProduto();
+            }
+        }
+
+        public void RemoverProdutos() {
+            Console.WriteLine("Deseja remover quantos produtos?");
+            int remove = int.Parse(Console.ReadLine());
+
+            if (remove > quantidade) {
+                Console.WriteLine("Quantidade de produtos menor que a quantidade à retirar.");
+            } else if (remove < 0) {
+                Console.WriteLine("A quantidade a ser retirada não pode ser negativa.");
+            } else {
+                quantidade -= remove;
+                DadosDoProduto();
+            }
+        }
+    }
+}
---
+++ Studies/C#/curso c# udemy/EstoqueDeLoja/EstoqueDeLoja/Program.cs
@@ -0,0 +1,63 @@
+﻿namespace EstoqueDeLoja {
+    class Program {
+        static void Main(string[] args) {
+            Produto p1 = new Produto();
+
+            Console.WriteLine("Digite os seguintes valores de estoque:");
+             p1.nome = ReadString("Digite o nome: ");
+             p1.valor = ReadDouble("Digite o valor: ");
+             p1.quantidade = ReadInt("Digite a quantidade: ");
+
+            p1.DadosDoProduto();
+
+            Console.WriteLine();
+            while (true) {
+                Console.WriteLine("Deseja adicionar ou remover algum produto?");
+                Console.WriteLine("1. Adicionar\n2. Remover\n3. Visualizar\n4. Sair");
+                Console.Write("Escolha: "); int escolha = int.Parse(Console.ReadLine());
+
+                switch (escolha) {
+                    case 1:
+                        p1.AdicionarProdutos();
+                        break;
+                    case 2:
+                        p1.RemoverProdutos();
+                        break;
+                    case 3:
+                        p1.DadosDoProduto();
+                        break;
+                    case 4:
+                        return;
+                    default:
+                        Console.WriteLine("Numero invalido, Tente novamente");
+                        break;
+                }
+            }
+        }
+
+        static string ReadString(string prompt) {
+            Console.Write(prompt);
+            return Console.ReadLine();
+        }
+
+        static double ReadDouble(string prompt) {
+            double result;
+            Console.Write(prompt);
+            while (!double.TryParse(Console.ReadLine(), out result) || result < 0) {
+                Console.WriteLine("Entrada invalida, tente novamente...");
+                Console.Write(prompt);
+            }
+            return result;
+        }
+
+        static int ReadInt(string prompt) { 
+            int result;
+            Console.Write(prompt);
+            while (!int.TryParse(Console.ReadLine(), out result) || (result < 0)) {
+                Console.WriteLine("Entrada invalida, tente novamente...");
+                Console.Write(prompt);
+            }
+            return result;
+        }
+    }
+}
