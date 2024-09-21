bfb342f22da1c5f257f12f7a94c293c29e7a8acf
Gabriel Machado <137454944+GabrielGMachado@users.noreply.github.com>
2024-09-20 21:50:33 -0300

    Add files via upload

---
+++ Studies/C#/basic exercices/dates/ConsoleApp1/Program.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Globalization;
+
+namespace datas {
+    class Program {
+        static void Main(string[] args) {
+            DateTime data = DateTime.UtcNow;
+
+            DateTime novadata = data.AddMinutes(3200);
+            novadata = data.AddHours(320);
+            novadata = data.AddDays(32);
+
+            Console.WriteLine(data);
+            Console.WriteLine(novadata);
+
+        }
+    }
+}
+
