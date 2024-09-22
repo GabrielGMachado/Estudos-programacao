ee22dcf819ca10cc10a9c24628597dcfb8b71377
Gabriel Machado <137454944+GabrielGMachado@users.noreply.github.com>
2024-09-22 15:04:26 -0300

    Add files via upload

---
+++ Studies/C#/basic exercices/HashSet/AprendimentosFoda/Program.cs
@@ -0,0 +1,15 @@
+﻿namespace HashSet {
+    class Program {
+        static void Main() { 
+            HashSet<int> a = new HashSet<int>();
+
+            a.Add(1);
+            a.Add(2);
+            a.Add(1);
+
+            Console.WriteLine(a.Count);
+
+            // HashSet são quase iguais a lista, porem ele é uma lista de numero unicos, então ele ignora a segunda vez que tentei adicionar o numero 1
+        }
+    }
+}
