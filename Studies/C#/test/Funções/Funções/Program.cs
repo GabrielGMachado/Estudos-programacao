b647b59615be0c252251e222bb23623f83492432
Gabriel Machado <137454944+GabrielGMachado@users.noreply.github.com>
2024-09-22 15:27:37 -0300

    Add files via upload

---
+++ Studies/C#/test/Funções/Funções/Program.cs
@@ -0,0 +1,23 @@
+﻿namespace Funcoes{
+    class Program {
+        static void Main() {
+            
+            int soma1 = Soma(7, 3);
+            Console.WriteLine(soma1);
+            
+            var resultado = SomaNome(7, 3);
+            Console.WriteLine(resultado.Soma);
+            Console.WriteLine(resultado.Nome);
+
+            //ou
+
+            (int soma, string nome) = SomaNome(7, 3); 
+            Console.WriteLine(soma);
+            Console.WriteLine(nome);
+        }
+
+        static int Soma(int v1, int v2) => v1 + v2; //função de uma linha
+
+        static (int Soma, string Nome) SomaNome(int v1, int v2) => (v1 + v2, "Gabriel"); // Função que retorna dois valores
+    }
+}
